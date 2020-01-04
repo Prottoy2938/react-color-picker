12fdca7c0b9112f1cd333b947f65162f38e42ffa
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-01-04 16:37:35 +0600

    Final Commit, fixed some small bug

--- src/ColorPickerForm.js
+++ src/ColorPickerForm.js
@@ -52,16 +52,16 @@ class ColorPickerForm extends Component {
         />
         <ValidatorForm
           onSubmit={this.handleSubmit}
-          ref='form'
+          ref="form"
           instantValidate={false}
         >
           <TextValidator
             value={newColorName}
             className={classes.colorNameInput}
-            placeholder='Color Name'
-            name='newColorName'
-            variant='filled'
-            margin='normal'
+            placeholder="Color Name"
+            name="newColorName"
+            variant="filled"
+            margin="normal"
             onChange={this.handleChange}
             validators={["required", "isColorNameUnique", "isColorUnique"]}
             errorMessages={[
@@ -71,9 +71,9 @@ class ColorPickerForm extends Component {
             ]}
           />
           <Button
-            variant='contained'
-            type='submit'
-            color='primary'
+            variant="contained"
+            type="submit"
+            color="primary"
             disabled={paletteIsFull}
             className={classes.addColor}
             style={{
